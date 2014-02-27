1783fe3f968291ea6721c73626cea56c23d28324
ElectroDragon <electrodragon@me.com>
2014-02-27 10:05:17 +0900

    日付を切り替えられるようにするための準備

--- Assets/Kanda/Scripts/Network/JAXAOpenAPICollector.cs
+++ Assets/Kanda/Scripts/Network/JAXAOpenAPICollector.cs
@@ -7,14 +7,21 @@ public class JAXAOpenAPICollector : MonoBehaviour {
 
 	private JAXAOpenAPI api;
 	public DataType[] requestTypes;
+	public DateTime[] availableDates;
 
 	IEnumerator Start () {
+		availableDates = new DateTime[] {
+			new DateTime(2012, 10, 1)
+			, new DateTime(2013, 1, 1)
+			, new DateTime(2013, 4, 1)
+			, new DateTime(2013, 7, 1)
+		};
 		while (JAXAOpenAPI.Instance == null) {
 			yield return null;
 		}
 		api = JAXAOpenAPI.Instance;
 		foreach (var type in requestTypes) {
-			api.CollectData(type, new DateTime(2012, 10, 1));
+			api.CollectData(type, availableDates[3]);
 		}
 	}
 }
